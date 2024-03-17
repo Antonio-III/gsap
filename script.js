485baf8ece787d7ad9499c204d4d19ae0180c86a
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2024-03-17 18:04:31 +0400

    add gsap 2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // gsap.from("sidebar-left", {duration: 1, x: "-100%", delay: 1.5 })
 // gsap.from("sidebar-right", {duration: 1, x: "-100vw", delay: 1, ease: "power2.in" })
 
-// We can move TO a location. (`.from` modifies BEFORE state, `.to` modifies TARGET state)
+// We can use `.to`. (`.from` modifies BEFORE state, `.to` modifies TARGET state)
 // when using `.from`, the TARGET state is the position of an element without GSAP
 // animation 4: Footer moves to target position "elasticly"
 // gsap.to("footer", {duration: 1, y: 0, ease: "elastic", delay: 2.5})
@@ -21,22 +21,23 @@
 
 // We can have timelines, so we don't have to make a lot of changes if we modify the delay of 1 animation.
 // What it does it chains animations after another. Meaning, an animation only occurs as soon as the last animation finishes. Adding `delay` is unnecessary at this point.
-const timeline = gsap.timeline({defaults: {duration: 1} })
+const timeline = gsap.timeline({defaults: {duration: 1,  } })
 
 timeline
-    .from("header", {y: "-200%", ease: "bounce", })
+    .from("header", {y: "-100%", ease: "bounce", display: "none"})
     .from("li", {opacity: 0, delay: 1, stagger: .5})
 
     // `.from` takes a second arg, which can be either `absolute` or `relative` delay. 
     // `Absolute` delay is as soon as the timeline starts. 
     // `Relative` delay is as soon as the last animation finishes. 
     // We've picked `absolute` delay (`1`), so the delay is of this animation is not after the last animation finishes, but rather 1 second after the timeline begins. 
-    .from("sidebar-right", {x: "-100vw", ease: "power2.in" }, 1)
-    // This uses `relative` delay. Left caret refers to when last animation starts, and the value is the delay when the animation starts. So only using a single left caret means to start as soon as the last animation starts.
-    .from("sidebar-left", {x: "-200%",  }, "<.5")
+    .from("sidebar-right", {x: "-100vw", ease: "power2.in", display: "none"}, 1)
 
-    .to("footer", {y: 0, ease: "elastic", }, )
-    .fromTo("button", {opacity: 0, scale: 0, rotation: 720}, {opacity: 1, scale: 2, rotation: 0})
+    // This uses `relative` delay. Left caret refers to when last animation starts, and the value is the delay when the animation starts. So only using a single left caret means to start as soon as the last animation starts.
+    .from("sidebar-left", {x: "-100%", display: "none"}, "<.5")
+
+    .from(".footer-one", {y: "-100%", ease: "elastic", display: "none"}, )
+    .fromTo("button", {opacity: 0, scale: 0, rotation: 720}, {opacity: 1, scale: 2, rotation: 0,})
 
 
 
@@ -46,4 +47,8 @@ const button = document.querySelector("button")
 
 button.addEventListener("click", () => { 
     timeline.timeScale(2)
-    timeline.reverse() })
+    timeline.reverse()
+    })
+
+// Code above this line is part of `GSAP 1`.
+// Code below this line is part of `Section 2`
